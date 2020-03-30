111d541b8b491ffcc29a5e6bab4ec48b8f971b4e
sanchezpepe <sanchezpepe97@gmail.com>
2020-03-30 17:57:41 -0600

    added methods to queue

--- dataStructures/queue/index.js
+++ dataStructures/queue/index.js
@@ -1,25 +1,23 @@
-// --- Description
-// Create a queue data structure.  The queue
-// should be a class with methods 'add' and 'remove'.
-// Adding to the queue should store an element until
-// it is removed
-// --- Examples
-//     const q = new Queue();
-//     q.add(1);
-//     q.remove(); // returns 1;
-
 class Queue {
-  constructor() {
-    this.data = [];
-  }
+	constructor() {
+		this.data = [];
+	}
 
-  add(record) {
-    this.data.unshift(record);
-  }
+	add(record) {
+		this.data.unshift(record);
+	}
 
-  remove() {
-    return this.data.pop();
-  }
+	remove() {
+		return this.data.pop();
+	}
+
+	peek() {
+		return this.data[this.data.length-1];
+	}
+
+	isEmpty(){
+		return this.data === 0;
+	}
 }
 
 module.exports = Queue;
