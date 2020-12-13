4e6e5e3f3743aa3549fd198b420ff975c7288dbf
Jose Sanchez <sanchezpepe97@gmail.com>
2020-12-13 13:54:39 -0600

    test

--- AlgoExpert/ARR_01_TwoNumberSum.java
+++ AlgoExpert/ARR_01_TwoNumberSum.java
@@ -1,15 +1,16 @@
 import java.util.*;
 
 class Program {
-  public static int[] twoNumberSum(int[] array, int targetSum) {
-    HashSet<Integer> seen = new HashSet<Integer>();
-		for(int actual : array){
-			if(seen.contains(targetSum - actual)){
-				return new int[]{actual, (targetSum - actual)};
-			} else {
-				seen.add(actual);
-			}
-		}
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+	    HashSet<Integer> seen = new HashSet<Integer>();
+	    for(int actual : array){
+            if(seen.contains(targetSum - actual)){
+                return new int[]{actual, (targetSum - actual)};
+            } else {
+                seen.add(actual);
+            }
+        }
+        
     return new int[0];
   }
 }
