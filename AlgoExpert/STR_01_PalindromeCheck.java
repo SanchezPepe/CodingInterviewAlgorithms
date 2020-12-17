e74441db246b1401ccd809c5841a9756b34c29d6
Jose Sanchez <sanchezpepe97@gmail.com>
2020-12-16 21:00:17 -0600

    added palindorme check

---
+++ AlgoExpert/STR_01_PalindromeCheck.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Program {
+  public static boolean isPalindrome(String str) {
+    //Pointers
+		int start = 0;
+		int end = str.length() - 1;
+		
+		while(start < end){
+			if(str.charAt(start) != str.charAt(end)) 
+				return false;
+			
+			start++;
+			end--;
+		}
+		
+    return true;
+  }
+}
