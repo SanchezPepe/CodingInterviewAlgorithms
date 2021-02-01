87765ceba27bfbcd20b5fa34e3d265c4160be875
Jose Sanchez <sanchezpepe97@gmail.com>
2021-02-01 15:27:39 -0600

    test identation fix

--- AlgoExpert/STR_02_CaesarCipherEncryptor.java
+++ AlgoExpert/STR_02_CaesarCipherEncryptor.java
@@ -1,21 +1,21 @@
 import java.util.*;
 
 class Program {
-  public static String caesarCypherEncryptor(String str, int key) {
+    public static String caesarCypherEncryptor(String str, int key) {
 
-		if(key > 26) key = key % 26;
-		if(key == 0) return str;
+        if (key > 26) key = key % 26;
+        if (key == 0) return str;
 
-		StringBuilder ans = new StringBuilder();
-		
-		for(int i = 0; i < str.length(); i++){
-			char c = str.charAt(i);
-			int asciiValue = c + key;
-			if(asciiValue > 122) 
-				asciiValue = (asciiValue % 122) + 96;
-			ans.append(Character.toString((char) asciiValue));
-		}
-		
-    return ans.toString();
-  }
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i<str.length(); i++) {
+            char c = str.charAt(i);
+            int asciiValue = c + key;
+            if (asciiValue > 122)
+                asciiValue = (asciiValue % 122) + 96;
+            ans.append(Character.toString((char) asciiValue));
+        }
+
+        return ans.toString();
+    }
 }
--- AlgoExpert/STR_04_LongestPalindromicSubstring.java
+++ AlgoExpert/STR_04_LongestPalindromicSubstring.java
@@ -17,43 +17,43 @@ import java.util.*;
 */
 
 class Program {
-	public static String longestPalindromicSubstring(String str) {
-		String longest = "";
-		String potential = "";
+    public static String longestPalindromicSubstring(String str) {
+        String longest = "";
+        String potential = "";
 
-		for (int i = 0; i<str.length(); i++) {
-			int left = (i == 0) ? i : i - 1;
-			int right = (i == str.length() - 1) ? i : i + 1;
+        for (int i = 0; i<str.length(); i++) {
+            int left = (i == 0) ? i : i - 1;
+            int right = (i == str.length() - 1) ? i : i + 1;
 
-			Character center = str.charAt(i);
-			Character leftChar = str.charAt(left);
-			Character rightChar = str.charAt(right);
+            Character center = str.charAt(i);
+            Character leftChar = str.charAt(left);
+            Character rightChar = str.charAt(right);
 
-			if (leftChar.equals(rightChar)) { // Odd size palindrome
-				right = i + 1;
-				potential = findPalindrome(str, left, right);
-				if (potential.length() > longest.length()) longest = potential;
+            if (leftChar.equals(rightChar)) { // Odd size palindrome
+                right = i + 1;
+                potential = findPalindrome(str, left, right);
+                if (potential.length() > longest.length()) longest = potential;
 
-			}
+            }
 
-			if (center.equals(leftChar)) { // Even size palindrome
-				right = i;
-				potential = findPalindrome(str, left, right);
-				if (potential.length() > longest.length()) longest = potential;
-			}
-		}
-		return longest;
-	}
+            if (center.equals(leftChar)) { // Even size palindrome
+                right = i;
+                potential = findPalindrome(str, left, right);
+                if (potential.length() > longest.length()) longest = potential;
+            }
+        }
+        return longest;
+    }
 
-	public static String findPalindrome(String str, int left, int right) {
-		int l = left;
-		int r = right;
+    public static String findPalindrome(String str, int left, int right) {
+        int l = left;
+        int r = right;
 
-		while (l >= 0 && r<str.length() && str.charAt(l) == str.charAt(r)) {
-			l--;
-			r++;
-		}
-		return str.substring(l + 1, r);
-	}
+        while (l >= 0 && r<str.length() && str.charAt(l) == str.charAt(r)) {
+            l--;
+            r++;
+        }
+        return str.substring(l + 1, r);
+    }
 
 }
